28f6c8746dc841eec18eb02081ac4e87fc9e25d6
wilsonchen <wilsonchen@superbank.com>
2019-05-31 14:48:15 +0800

    隱藏下排 更改btn樣式 js功能小整理

--- powpow/js/NoRefreshChange.js
+++ powpow/js/NoRefreshChange.js
@@ -12,37 +12,37 @@ $(".question button.next , .question button.last").on("click", function () {
 });
 
 $(".question button.go").on("click", function () {
-   //檢查所有欄位
-   var question1From = false;
-   $("input[name='question1']").each(function () {
-      if ($(this).prop("checked")) { question1From = true; }
-   });
-   if(!question1From){ changeDataPage(1);return false;}
-
-   var question2From = false ;
-   $("input[name='question2']").each(function () {
-      if ($(this).prop("checked")) { question2From = true; }
-   });
-   if(!question2From){ changeDataPage(2);return false;}
-
-   var question3From = false ;
-   $("input[name='question3']").each(function () {
-      if ($(this).prop("checked")) { question3From = true; }
-   });
-   if(!question3From){ changeDataPage(3);return false;}
-
    //判斷有無勾選
-   //inputChkTf("question1",1);
-   
+   var question8From = inputChkTf("question8", 7);
+   var question7From = inputChkTf("question7", 6);
+   var question6From = inputChkTf("question6", 5);
+   var question5From = inputChkTf("question5", 4);
+   var question4From = inputChkTf("question4", 4);
+   var question3From = inputChkTf("question3", 3);
+   var question2From = inputChkTf("question2", 2);
+   var question1From = inputChkTf("question1", 1);
+
+   if (question8From && question7From && question6From && question5From && question4From && question3From && question2From && question1From) {
+      console.log("go");
+   }
+   else {
+      console.log("not go");
+   }
 });
 
 //判斷input radio checkbox 有無勾選funtcion
-function inputChkTf(inputName,pageValue){
+function inputChkTf(inputName, pageValue) {
    var questionFrom = false;
-   $("input[name="+inputName+"]").each(function () {
+   $("input[name=" + inputName + "]").each(function () {
       if ($(this).prop("checked")) { questionFrom = true; }
    });
-   if(!questionFrom){ changeDataPage(pageValue);return false;}
+   if (!questionFrom) {
+      $("input[name=" + inputName + "]").each(function () {
+         $(this).next("label").addClass("error");
+      });
+      changeDataPage(pageValue);
+   }
+   return questionFrom;
 }
 
 //改變頁數funtcion
--- powpow/js/header.js
+++ powpow/js/header.js
@@ -1,2 +1,2 @@
-var headerHtml = ' <header><img src="" alt=""></header>';
+var headerHtml = ' <header><img src="https://cdn.pixabay.com/photo/2018/03/05/15/25/toys-3200969_960_720.jpg" alt=""></header>';
 document.write(headerHtml);
