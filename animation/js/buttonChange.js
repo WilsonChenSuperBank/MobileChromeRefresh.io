55f49564aac43fbc120c5fd5c157ce1091ee0599
wilsonchen <wilsonchen@superbank.com>
2019-06-04 11:14:10 +0800

    動畫練習

--- animation/js/buttonChange.js
+++ animation/js/buttonChange.js
@@ -2,3 +2,12 @@ $(".buttonVal").on("click",function(){
    $(".buttonVal").removeClass("active");
    $(this).addClass("active");
 })
+
+$(document).ready(function() {
+   $('.has-animation').each(function(index) {
+     $(this).delay($(this).data('delay')).queue(function(){
+       $(this).addClass('animate-in');
+     });
+   });
+ });
+ 
